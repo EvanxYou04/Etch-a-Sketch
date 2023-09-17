911eaaa7eaac04bcaea81301824bd2426caaad53
Evan <evanyou2004@gmail.com>
2023-09-17 15:24:28 -0500

    reformatted code.

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 console.log("hello world");
-
-function red() { 
-    console.log("red");
+let paint = "#ffffff"; // default color
+function red() {
+  console.log("red");
 }
-function blue() { 
-    console.log("blue");
+function blue() {
+  console.log("blue");
 }
-function green() { 
-    console.log("green");
+function green() {
+  console.log("green");
 }
 
 function clear() {
-    console.log('clear');
+  console.log("clear");
 }
 
 const container = document.querySelector(".canvas-container");
 
 function makeRows(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (c = 0; c < (rows * cols); c++) {
-      let cell = document.createElement("div");
-      cell.innerText = (c + 1);
-      container.appendChild(cell).className = "grid-item";
-    };
-  };
-  
-  makeRows(2,2);
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
+  for (c = 0; c < rows * cols; c++) {
+    let cell = document.createElement("div");
+    cell.innerText = c + 1;
+    container.appendChild(cell).className = "grid-item";
+  }
+}
+
+makeRows(2, 2);
