b6d15acc6ae9ba043e3e9c9dbe7e044abb73da1a
Evan <evanyou2004@gmail.com>
2023-09-17 19:38:13 -0500

    added clear screen functionality.

--- main.js
+++ main.js
@@ -1,38 +1,39 @@
-const size = 2
+let size = 16; //starting size
+let paint = '#ffffff'; // default color
+let mouseDown = false;
 
-let paint = '#f1efef' // default color
-let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
-document.body.onmousedown = () => (mouseDown = true)
-document.body.onmouseup = () => (mouseDown = false)
+const container = document.querySelector('.canvas-container');
+const colorPicker = document.querySelector('#colorPicker');
+const clearButton = document.querySelector('#clearBtn');
 
-function clear() {
-    container.innerHTML = ''
-    makeGrid(size)
-}
-
-const container = document.querySelector('.canvas-container')
-const colorPicker = document.querySelector('#colorPicker')
-
-colorPicker.addEventListener('change', updatePaint)
+clearButton.addEventListener('click', () => {
+    console.log('clear');
+    container.innerHTML = '';
+    makeGrid(size);
+});
+colorPicker.addEventListener('change', updatePaint);
 
 function makeGrid(sideLen) {
-    container.style.setProperty('--grid-rows', sideLen)
-    container.style.setProperty('--grid-cols', sideLen)
+    container.style.setProperty('--grid-rows', sideLen);
+    container.style.setProperty('--grid-cols', sideLen);
     for (c = 0; c < sideLen * sideLen; c++) {
-        let cell = document.createElement('div')
-        container.appendChild(cell).className = 'grid-item'
-        cell.addEventListener('mousedown', changeColor)
-        cell.addEventListener('mouseover', changeColor)
+        let cell = document.createElement('div');
+        cell.addEventListener('mouseover', changeColor);
+        cell.addEventListener('mousedown', changeColor);
+        container.appendChild(cell).className = 'grid-item';
     }
 }
 
 function updatePaint(e) {
-    paint = e.target.value
-    console.log(paint)
+    paint = e.target.value;
+    console.log(paint);
 }
 
 function changeColor(e) {
-    e.target.style.backgroundColor = paint
+    if (e.type === 'mouseover' && !mouseDown) return;
+    e.target.style.backgroundColor = paint;
 }
-makeGrid(16)
+makeGrid(16);
