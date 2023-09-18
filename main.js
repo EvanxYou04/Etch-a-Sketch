b4402143dc0585967a17f842ef6a6a30f1022121
Evan <evanyou2004@gmail.com>
2023-09-17 19:14:48 -0500

    updated formating of file.

--- main.js
+++ main.js
@@ -1,37 +1,38 @@
-const size = 2;
+const size = 2
 
-let paint = "#ffffff"; // default color
-let mouseDown = false;
+let paint = '#f1efef' // default color
+let mouseDown = false
 
-document.body.onmousedown = () => (mouseDown = true);
-document.body.onmouseup = () => (mouseDown = false);
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
 function clear() {
-  paint = "#ffffff";
+    container.innerHTML = ''
+    makeGrid(size)
 }
 
-const container = document.querySelector(".canvas-container");
-const colorPicker = document.querySelector("#colorPicker");
+const container = document.querySelector('.canvas-container')
+const colorPicker = document.querySelector('#colorPicker')
 
-colorPicker.addEventListener("change", updatePaint);
+colorPicker.addEventListener('change', updatePaint)
 
 function makeGrid(sideLen) {
-  container.style.setProperty("--grid-rows", sideLen);
-  container.style.setProperty("--grid-cols", sideLen);
-  for (c = 0; c < sideLen * sideLen; c++) {
-    let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
-    cell.addEventListener("mousedown", changeColor);
-    cell.addEventListener("mouseover", changeColor);
-  }
+    container.style.setProperty('--grid-rows', sideLen)
+    container.style.setProperty('--grid-cols', sideLen)
+    for (c = 0; c < sideLen * sideLen; c++) {
+        let cell = document.createElement('div')
+        container.appendChild(cell).className = 'grid-item'
+        cell.addEventListener('mousedown', changeColor)
+        cell.addEventListener('mouseover', changeColor)
+    }
 }
 
 function updatePaint(e) {
-  paint = e.target.value;
-  console.log(paint);
+    paint = e.target.value
+    console.log(paint)
 }
 
 function changeColor(e) {
-  e.target.style.backgroundColor = paint;
+    e.target.style.backgroundColor = paint
 }
-makeGrid(16);
+makeGrid(16)
